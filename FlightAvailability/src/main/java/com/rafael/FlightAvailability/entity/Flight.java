391d40f6881722faff83a86ee6518bff60b9d0a3
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-06-13 11:22:05 -0300

    feat: Create Flight entity

---
+++ FlightAvailability/src/main/java/com/rafael/FlightAvailability/entity/Flight.java
@@ -0,0 +1,15 @@
+package com.rafael.FlightAvailability.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Flight {
+	
+	private String flightId;
+}
